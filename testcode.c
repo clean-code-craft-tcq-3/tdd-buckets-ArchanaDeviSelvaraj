b248a4ce4cb6b95864f93debdb24a0f7fb43cbc9
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-07 15:35:26 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -4,7 +4,7 @@
 int main()
 {
 //TDD Step 1: Write test code
-int currentsamplesarray[7] = {3, 3, 5, 4, 10, 11, 12};
+int currentsamplesarray[] = {3, 3, 5, 4, 10, 11, 12};
 assert(DetectContinuousRanges(currentsamplesarray) == 2);
 //End of step 1 error observed as expected : error: ‘DetectContinuousRanges’ was not declared in this scope
 return 0;
