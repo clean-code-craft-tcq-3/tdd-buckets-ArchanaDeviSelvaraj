2d13ce587521cb17bc533346a390585e5c35fbe7
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-07 15:22:50 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -1,1 +1,9 @@
+#include <assert.h>
 
+int main()
+{
+//Step 1: Write test code
+int currentsamplesarray[7] = {3, 3, 5, 4, 10, 11, 12};
+assert(DetectContinuousRanges(currentsamplesarray) == 2);
+return 0;
+}
