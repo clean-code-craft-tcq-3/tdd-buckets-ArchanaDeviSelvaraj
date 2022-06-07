224cbfe982d4579455e68b17855e6396c0f05c48
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-07 15:37:37 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -1,5 +1,7 @@
 #include "CurrentMeasurementRange.h"
 int DetectContinuousRanges(int currentsamplesarray[])
 {
+  return 0;
   //TDD Step2 : Just added fucntion to make code compile
+  //end of step2 as expected test failed since actual code implementation not yet done : Assertion `DetectContinuousRanges(currentsamplesarray) == 2' failed.
 }
