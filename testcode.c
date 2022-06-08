40f816fb469aa0862daeebd717533414001082f0
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 11:43:17 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -5,7 +5,7 @@ int main()
 {
 //TDD Step 1: Write test code
 int samplesarray[] = {3, 3, 5, 4, 10, 11, 12};
-int arraysize = sizeof(currentsamplesarray) / sizeof(currentsamplesarray[0]);
+int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
 printf("Size of the array is: %d\n", arraysize);
 //assert(DetectContinuousRanges(currentsamplesarray) == 2);
  //DetectContinuousRanges(samplesarray);
