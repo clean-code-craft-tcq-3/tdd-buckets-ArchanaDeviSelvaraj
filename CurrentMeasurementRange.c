b11573dee0f82cb19f5e4aecfb4d18d4599908ff
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 17:23:47 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -10,20 +10,17 @@ int DetectContinuousRanges(int currentsamplesarray[],int arraysize)
   //as expected at end of step3 test case got passed
   int startvalue=currentsamplesarray[0],endvalue=currentsamplesarray[0],rangecount=0,consecutivecount=0;
   qsort(currentsamplesarray, arraysize, sizeof(int), cmpfunc);
-  for( int n = 0 ; n < arraysize; n++ ) 
+  for( int index = 0 ; index < arraysize; index++ ) 
   {   
       int diff = currentsamplesarray[n+1] - currentsamplesarray[n];
       int consecutive=0;
-      //printf("Diff value %d\n",diff);
       if((diff==0) || (diff==1))
       {
        consecutive=1;
-       //printf("consecutive value %d\n",consecutive);
       }
      if(consecutive==1)
      {
         consecutivecount++;
-        //printf("consecutivecount value %d\n",consecutivecount);
         endvalue = currentsamplesarray[n+1];
      }
      else if(consecutivecount!=0)
@@ -36,7 +33,6 @@ int DetectContinuousRanges(int currentsamplesarray[],int arraysize)
         consecutivecount=0;
      }
   }
-  //printf("Consecutive count %d\n",consecutivecount);
   printf("Total number of continuous ranges detected : %d\n",rangecount);   
   return rangecount;
   //TDD Step2 : Just added function to make code compile
