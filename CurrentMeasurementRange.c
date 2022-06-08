5779caee136206b5ffdd6b25d73c11ddace80db2
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 17:39:49 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -8,7 +8,7 @@ int cmpfunc (const void * value1, const void * value2) {
 int isConsecutive(int diff)
 {
    if((diff==0) || (diff==1))
-       consecutive=1;
+     int consecutive=1;
    return consecutive;
 }
 
