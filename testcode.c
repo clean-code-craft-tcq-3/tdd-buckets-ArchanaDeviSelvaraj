b2cff786945732800c7e715afb56c08ea7a04b96
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 13:08:03 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -7,8 +7,8 @@ int main()
 int samplesarray1[] = {3, 3, 5, 4, 10, 11, 12};
 int arraysize1 = sizeof(samplesarray1) / sizeof(samplesarray1[0]);
 printf("Size of the array is: %d\n", arraysize1);
-assert(DetectContinuousRanges(samplesarray1,arraysize1) == 2);
- //DetectContinuousRanges(samplesarray,arraysize);
+//assert(DetectContinuousRanges(samplesarray1,arraysize1) == 2);
+ DetectContinuousRanges(samplesarray1,arraysize1);
 //End of step 1 error observed as expected : error: ‘DetectContinuousRanges’ was not declared in this scope
 int samplesarray2[] = {3, 30, 5, 7, 10, 15, 12, 20};
 int arraysize2 = sizeof(samplesarray2) / sizeof(samplesarray2[0]);
