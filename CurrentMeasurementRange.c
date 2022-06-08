1fb28dc569a6c9a7693ab64ff3f51178fcee1644
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 10:51:13 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -1,6 +1,12 @@
 #include "CurrentMeasurementRange.h"
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int DetectContinuousRanges(int currentsamplesarray[])
 {
+  qsort(currentsamplesarray, sizeof(currentsamplesarray), sizeof(int), cmpfunc);
   return 0;
   //TDD Step2 : Just added function to make code compile
   //end of step2 as expected test failed since actual code implementation not yet done : Assertion `DetectContinuousRanges(currentsamplesarray) == 2' failed.
