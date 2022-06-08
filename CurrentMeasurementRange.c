d0c0225fad59e719ce64a72a0fcfafaff19386e9
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-08 18:04:17 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -38,7 +38,7 @@ int DetectContinuousRanges(int currentsamplesarray[],int arraysize)
   //TDD Step3:Code Implementation done to make the test pass
   //as expected at end of step3 test case got passed
   qsort(currentsamplesarray, arraysize, sizeof(int), cmpfunc);
-  int startvalue=currentsamplesarray[0],endvalue=currentsamplesarray[0];
+  int startvalue=currentsamplesarray[0],endvalue=currentsamplesarray[0],rangecount=0,consecutivecount=0;
   for( int index = 0 ; index < arraysize; index++ ) 
   {   
       int diff = currentsamplesarray[index+1] - currentsamplesarray[index];
