a891f13c8754c68fab2bfc1a8014d0b3161ab94c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-09 09:59:50 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -5,11 +5,15 @@ int main()
 {
 //TDD Step 1: Write test code
 printf("Test case 1\n");
+int samplesarray[] = {4,5};
+int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
+assert(DetectandPrintContinuousRanges(samplesarray,arraysize) == 2);
+//End of step 1 error observed as expected : error: ‘DetectContinuousRanges’ was not declared in this scope
+printf("\nTest case 2\n");
 int samplesarray1[] = {3, 3, 5, 4, 10, 11, 12};
 int arraysize1 = sizeof(samplesarray1) / sizeof(samplesarray1[0]);
 assert(DetectandPrintContinuousRanges(samplesarray1,arraysize1) == 2);
-//End of step 1 error observed as expected : error: ‘DetectContinuousRanges’ was not declared in this scope
-printf("\nTest case 2\n");
+printf("\nTest case 3\n");
 int samplesarray2[] = {3, 30, 5, 7, 10, 15, 12, 20};
 int arraysize2 = sizeof(samplesarray2) / sizeof(samplesarray2[0]);
 assert(DetectandPrintContinuousRanges(samplesarray2,arraysize2) == 0);
