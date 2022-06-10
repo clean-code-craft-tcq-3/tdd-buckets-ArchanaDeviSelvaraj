4a672dcaa3fa3eae8160b71b6f125147b3c3fabf
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-10 12:31:21 +0530

    Update CurrentMeasurementRange.h

--- CurrentMeasurementRange.h
+++ CurrentMeasurementRange.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
-int DetectRangecountandPrintdetails(int currentsamplesarray[],int arraysize);
+int detectRangeCountAndPrintDetails(int currentsamplesarray[],int arraysize);
 int isConsecutive(int diff);
