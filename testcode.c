6afbb68663d3dbb1998f1b3549de569c1b3b31b2
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-10 17:45:26 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -3,21 +3,20 @@
 
 int main()
 {
-Range RangeinfoOutput[50];
 printf("Test case 1");
 int samplesarray[] = {4,5};
 int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
-RangeinfoOutput[] = detectRangeCountAndPrintDetails(samplesarray,arraysize);
-assert(RangeinfoOutput[].rangecount == 1);
-assert(RangeinfoOutput[1].startvalue == 4);
-assert(RangeinfoOutput[1].endvalue == 5);  
+Rangeinfo[] = detectRangeCountAndPrintDetails(samplesarray,arraysize);
+assert(Rangeinfo[].rangecount == 1);
+assert(Rangeinfo[1].startvalue == 4);
+assert(Rangeinfo[1].endvalue == 5);  
 printf("\n\nTest case 2");
 int samplesarray1[] = {3, 3, 5, 4, 10, 11, 12};
 int arraysize1 = sizeof(samplesarray1) / sizeof(samplesarray1[0]);
-RangeinfoOutput[] = detectRangeCountAndPrintDetails(samplesarray,arraysize);
-assert(RangeinfoOutput[].rangecount == 2);
-assert(RangeinfoOutput[1].startvalue == 3);
-assert(RangeinfoOutput[1].endvalue == 5);  
+//RangeinfoOutput[] = detectRangeCountAndPrintDetails(samplesarray,arraysize);
+//assert(RangeinfoOutput[].rangecount == 2);
+//assert(RangeinfoOutput[1].startvalue == 3);
+//assert(RangeinfoOutput[1].endvalue == 5);  
 //assert(detectRangeCountAndPrintDetails(samplesarray1,arraysize1) == 2);
 printf("\n\nTest case 3");
 int samplesarray2[] = {3, 30, 5, 7, 10, 15, 12, 20};
