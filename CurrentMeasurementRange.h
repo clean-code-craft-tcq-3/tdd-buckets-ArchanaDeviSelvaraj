657521f134fd1591b9632731643d56f728b8ea6b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-10 17:44:25 +0530

    Update CurrentMeasurementRange.h

--- CurrentMeasurementRange.h
+++ CurrentMeasurementRange.h
@@ -4,6 +4,6 @@ typedef struct Range
 {
    int startvalue; int endvalue; int rangecount;
 }Range;
-
+Range Rangeinfo[50];
 Range detectRangeCountAndPrintDetails(int currentsamplesarray[],int arraysize);
 int isConsecutive(int diff);
