27df34eb03c8801d0d042bcc0b4a2ee14c3dd7c1
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-14 14:23:19 +0530

    Update CurrentMeasurementRange.c

--- CurrentMeasurementRange.c
+++ CurrentMeasurementRange.c
@@ -64,5 +64,5 @@ Range detectRangeCountAndPrintDetails(int currentsamplesarray[],int arraysize)
   }
   Rangeinfo[rangecount].rangecount = rangecount;
   printNoRangeFound(Rangeinfo[rangecount].rangecount);
-  return &Rangeinfo[rangecount];
+  return &Rangeinfo;
 }
