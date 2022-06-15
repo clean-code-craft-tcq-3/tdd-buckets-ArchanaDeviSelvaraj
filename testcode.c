c24f01331eae52447aec4ac86fcea7d61f27325b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-15 14:55:59 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -9,7 +9,7 @@ int rangeinfo[100][4];
 printf("Test case 1");
 int samplesarray[] = {4,5};
 int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray,arraysize);
+ptr = detectAndPrintRangeDetails(samplesarray,arraysize);
 //Test case to check start of range
 assert(*(ptr+4) == 4);
 //Test case to check end of range
@@ -22,7 +22,7 @@ assert(*(ptr+7) == 1);
 printf("\n\nTest case 2");
 int samplesarray1[] = {3, 3, 5, 4, 10, 11, 12};
 int arraysize1 = sizeof(samplesarray1) / sizeof(samplesarray1[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray1,arraysize1);
+ptr = detectAndPrintRangeDetails(samplesarray1,arraysize1);
 //Test case to check start of range
 assert(*(ptr+4) == 3);
 assert(*(ptr+8) == 10);
@@ -38,14 +38,14 @@ assert(*(ptr+11) == 2);
 printf("\n\nTest case 3");
 int samplesarray2[] = {3, 30, 5, 7, 10, 15, 12, 20};
 int arraysize2 = sizeof(samplesarray2) / sizeof(samplesarray2[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray2,arraysize2);
+ptr = detectAndPrintRangeDetails(samplesarray2,arraysize2);
 //Test case to check number of range detected
 assert(*(ptr+3) == 0); 
   
 printf("\n\nTest case 4");
 int samplesarray3[] = {-3, -30, -5, -7, -10, -11, -12, 20, -6};
 int arraysize3 = sizeof(samplesarray3) / sizeof(samplesarray3[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray3,arraysize3); 
+ptr = detectAndPrintRangeDetails(samplesarray3,arraysize3); 
 //Test case to check start of range
 assert(*(ptr+4) == -12);
 assert(*(ptr+8) == -7);
@@ -62,7 +62,7 @@ assert(*(ptr+11) == 2);
 printf("\n\nTest case 5");
 int samplesarray4[] = {-2,-1,0,1,2,3,4,5};
 int arraysize4 = sizeof(samplesarray4) / sizeof(samplesarray4[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray4,arraysize4);
+ptr = detectAndPrintRangeDetails(samplesarray4,arraysize4);
 //Test case to check start of range
 assert(*(ptr+4) == -2);
 //Test case to check end of range
@@ -75,7 +75,7 @@ assert(*(ptr+7) == 1);
 printf("\n\nTest case 6");
 int samplesarray5[] = {};
 int arraysize5 = sizeof(samplesarray5) / sizeof(samplesarray5[0]);
-ptr = detectRangeCountAndPrintDetails(samplesarray5,arraysize5);
+ptr = detectAndPrintRangeDetails(samplesarray5,arraysize5);
 //Test case to check number of range detected
 assert(*(ptr+3) == 0); 
   
