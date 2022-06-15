b78e365edb58d16869d11741186a5ef2f8b2a543
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-15 11:05:36 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -41,16 +41,16 @@ assert(*(ptr+3) == 0);
 printf("\n\nTest case 4");
 int samplesarray3[] = {-3, -30, -5, -7, -10, -11, -12, 20, -6};
 int arraysize3 = sizeof(samplesarray3) / sizeof(samplesarray3[0]);
-detectRangeCountAndPrintDetails(samplesarray3,arraysize3); 
+ptr = detectRangeCountAndPrintDetails(samplesarray3,arraysize3); 
 //Test case to check start of range
 assert(*(ptr+4) == -12);
-//assert(*(ptr+8) == -7);
+assert(*(ptr+8) == -7);
 //Test case to check end of range
 assert(*(ptr+5) == -10);
-//assert(*(ptr+9) == -5);
+assert(*(ptr+9) == -5);
 //Test case to check number of readings in the range
 assert(*(ptr+6) == 3);
-//assert(*(ptr+10) == 3);
+assert(*(ptr+10) == 3);
 //Test case to check number of range detected
 assert(*(ptr+11) == 2); 
 printf("\n\nTest case 5");
