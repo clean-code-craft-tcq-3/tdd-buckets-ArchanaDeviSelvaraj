b8935024fe363ab42c196efc8d6db9ca9da10714
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-15 12:50:16 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -5,6 +5,7 @@ int main()
 {
 int *ptr;
 int rangeinfo[100][4];
+  
 printf("Test case 1");
 int samplesarray[] = {4,5};
 int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
@@ -17,6 +18,7 @@ assert(*(ptr+5) == 5);
 assert(*(ptr+6) == 2);
 //Test case to check number of range detected
 assert(*(ptr+7) == 1);  
+  
 printf("\n\nTest case 2");
 int samplesarray1[] = {3, 3, 5, 4, 10, 11, 12};
 int arraysize1 = sizeof(samplesarray1) / sizeof(samplesarray1[0]);
@@ -32,16 +34,19 @@ assert(*(ptr+6) == 4);
 assert(*(ptr+10) == 3);
 //Test case to check number of range detected
 assert(*(ptr+11) == 2);  
+  
 printf("\n\nTest case 3");
 int samplesarray2[] = {3, 30, 5, 7, 10, 15, 12, 20};
 int arraysize2 = sizeof(samplesarray2) / sizeof(samplesarray2[0]);
 ptr = detectRangeCountAndPrintDetails(samplesarray2,arraysize2);
 //Test case to check number of range detected
 assert(*(ptr+3) == 0); 
+  
 printf("\n\nTest case 4");
 int samplesarray3[] = {-3, -30, -5, -7, -10, -11, -12, 20, -6};
 int arraysize3 = sizeof(samplesarray3) / sizeof(samplesarray3[0]);
 ptr = detectRangeCountAndPrintDetails(samplesarray3,arraysize3); 
+  
 printf("\n\nTest case 5");
 int samplesarray4[] = {-2,-1,0,1,2,3,4,5};
 int arraysize4 = sizeof(samplesarray4) / sizeof(samplesarray4[0]);
@@ -54,6 +59,7 @@ assert(*(ptr+5) == 5);
 assert(*(ptr+6) == 8);
 //Test case to check number of range detected
 assert(*(ptr+7) == 1);  
+  
 printf("\n\nTest case 6");
 int samplesarray5[] = {};
 int arraysize5 = sizeof(samplesarray5) / sizeof(samplesarray5[0]);
