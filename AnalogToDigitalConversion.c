f58d369cd66ca61de375e2c30ab694c93517ccdd
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-30 14:57:27 +0530

    Update AnalogToDigitalConversion.c

--- AnalogToDigitalConversion.c
+++ AnalogToDigitalConversion.c
@@ -5,6 +5,5 @@ int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution)
    int maxA2Dvalue = pow(2,bitresolution) - 2;
    float convertedvalue = (float)maxAmp * (float)A2Dvalue/(float)maxA2Dvalue; 
    int Ampere = round(convertedvalue);
-   printf("\n Ampere is %d",Ampere);
    return Ampere;
 }
