b5273208c43bb54ed74b2d854fe95650e9f11484
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-30 14:41:41 +0530

    Update testcode.c

--- testcode.c
+++ testcode.c
@@ -86,6 +86,6 @@ assert(isConsecutive(1) == 1);
 assert(isConsecutive(5) == 0);
   
 //Test case for conversion check
-assert(convertA2DToAmpere(10,1146,4094) == 3);
+convertA2DToAmpere(10,1146,12);
 return 0;
 }
