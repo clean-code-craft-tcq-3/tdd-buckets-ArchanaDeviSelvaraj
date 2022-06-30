702b70b5940b640d4df301af10f9fc1d7476dc6b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-30 15:08:05 +0530

    Update AnalogToDigitalConversion.c

--- AnalogToDigitalConversion.c
+++ AnalogToDigitalConversion.c
@@ -1,9 +1,16 @@
 #include <math.h>
 #include <stdio.h>
+
+#define invalid -1;
+
 int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution)
 {
+   int Ampere = invalid;
    int maxA2Dvalue = pow(2,bitresolution) - 2;
+   if(A2Dvalue<=maxA2Dvalue)
+   {
    float convertedvalue = (float)maxAmp * (float)A2Dvalue/(float)maxA2Dvalue; 
-   int Ampere = round(convertedvalue);
+   Ampere = round(convertedvalue);
+   }
    return Ampere;
 }
