c572e1972652561fe3d51b0160ea9c35ea071981
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-06-30 18:15:40 +0530

    Update AnalogToDigitalConversion.c

--- AnalogToDigitalConversion.c
+++ AnalogToDigitalConversion.c
@@ -8,6 +8,7 @@ int Amperearray[] = {};
 int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution)
 {
    int Ampere = invalid;
+   A2Dvalue = abs(A2Dvalue);
    int maxA2Dvalue = pow(2,bitresolution) - 2;
    if(A2Dvalue<=maxA2Dvalue)
    {
@@ -23,7 +24,7 @@ void convertA2DToAmpereRange(int A2Dvaluearray[], int arraysize, int* Amperearra
    for(int index=0; index < arraysize; index++)
    {
      int Ampere = convertA2DToAmpere(maxAmp,A2Dvaluearray[index],bitresolution);
-      if(!(Ampere == invalid))
+      if(Ampere != -1)
       {
          Amperearray[count] = Ampere;
          count++;
